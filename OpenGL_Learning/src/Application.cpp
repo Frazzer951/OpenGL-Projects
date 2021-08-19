f1037f21429fc4ea801b1a334156a4f632140926
Luke Eltiste <luke343279@gmail.com>
2021-08-19 13:59:38 -0700

    add clang format and tidy

--- OpenGL_Learning/src/Application.cpp
+++ OpenGL_Learning/src/Application.cpp
@@ -1,14 +1,14 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 
 int main( void )
 {
   GLFWwindow * window;
 
   /* Initialize the library */
-  if( !glfwInit() )
-    return -1;
+  if( !glfwInit() ) return -1;
 
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
@@ -21,8 +21,7 @@ int main( void )
   /* Make the window's context current */
   glfwMakeContextCurrent( window );
 
-  if( glewInit() != GLEW_OK )
-    std::cout << "Error!" << std::endl;
+  if( glewInit() != GLEW_OK ) std::cout << "Error!" << std::endl;
 
   std::cout << glGetString( GL_VERSION ) << std::endl;
 
