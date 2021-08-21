993a14a4a87cafb5f4f9665282e2a4ba3c3032ce
Luke Eltiste <luke343279@gmail.com>
2021-08-21 10:10:15 -0700

    Add textures

--- OpenGL_Learning/src/Application.cpp
+++ OpenGL_Learning/src/Application.cpp
@@ -6,6 +6,7 @@
 #include "IndexBuffer.h"
 #include "Renderer.h"
 #include "Shader.h"
+#include "Texture.h"
 #include "VertexArray.h"
 #include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
@@ -42,24 +43,28 @@ int main( void )
   std::cout << glGetString( GL_VERSION ) << std::endl;
   {
     float positions[] = {
-      -0.5f, -0.5f,    // 0
-      +0.5f, -0.5f,    // 1
-      +0.5f, +0.5f,    // 2
-      -0.5f, +0.5f     // 3
+      -0.5f, -0.5f, +0.0f, +0.0f,    // 0
+      +0.5f, -0.5f, +1.0f, +0.0f,    // 1
+      +0.5f, +0.5f, +1.0f, +1.0f,    // 2
+      -0.5f, +0.5f, +0.0f, +1.0f     // 3
     };
     unsigned int indices[] = {
       0, 1, 2,
       2, 3, 0
     };
 
+    GLCall( glEnable( GL_BLEND ) );
+    GLCall( glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA ) );
+
     unsigned int vao;
     GLCall( glGenVertexArrays( 1, &vao ) );
     GLCall( glBindVertexArray( vao ) );
 
     VertexArray  va;
-    VertexBuffer vb( positions, 4 * 2 * sizeof( float ) );
+    VertexBuffer vb( positions, 4 * 4 * sizeof( float ) );
 
     VertexBufferLayout layout;
+    layout.Push<float>( 2 );
     layout.Push<float>( 2 );
     va.AddBuffer( vb, layout );
 
@@ -69,6 +74,10 @@ int main( void )
     shader.Bind();
 
     shader.SetUniform4f( "u_Color", 0.8f, 0.3f, 0.8f, 1.0f );
+
+    Texture texture( "res/textures/ChernoLogo.png" );
+    texture.Bind();
+    shader.SetUniform1i( "u_Texture", 0 );
 
     va.Bind();
     vb.Unbind();
