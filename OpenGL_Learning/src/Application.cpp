8fc32e36e35f476d45a1f89ee6b01640eccf5dda
Luke Eltiste <luke343279@gmail.com>
2021-08-21 18:12:24 -0700

    imgui

--- OpenGL_Learning/src/Application.cpp
+++ OpenGL_Learning/src/Application.cpp
@@ -16,6 +16,10 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 int main( void )
 {
   GLFWwindow * window;
@@ -73,16 +77,12 @@ int main( void )
 
     IndexBuffer ib( indices, 6 );
 
-    glm::mat4 proj  = glm::ortho( 0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f );
-    glm::mat4 view  = glm::translate( glm::mat4( 1.0f ), glm::vec3( -100, 0, 0 ) );
-    glm::mat4 model = glm::translate( glm::mat4( 1.0f ), glm::vec3( 200, 200, 0 ) );
-
-    glm::mat4 mvp = proj * view * model;
+    glm::mat4 proj = glm::ortho( 0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f );
+    glm::mat4 view = glm::translate( glm::mat4( 1.0f ), glm::vec3( -100, 0, 0 ) );
 
     Shader shader( "res/shaders/Basic.shader" );
     shader.Bind();
     shader.SetUniform4f( "u_Color", 0.8f, 0.3f, 0.8f, 1.0f );
-    shader.SetUniformMat4f( "u_MVP", mvp );
 
     Texture texture( "res/textures/ChernoLogo.png" );
     texture.Bind();
@@ -95,6 +95,16 @@ int main( void )
 
     Renderer renderer;
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO & io = ImGui::GetIO();
+    (void) io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL( window, true );
+    ImGui_ImplOpenGL3_Init( (char *) glGetString( GL_NUM_SHADING_LANGUAGE_VERSIONS ) );
+
+    glm::vec3 translation( 200, 200, 0 );
+
     float r         = 0.0f;
     float increment = 0.05f;
     /* Loop until the user closes the window */
@@ -103,8 +113,17 @@ int main( void )
       /* Render here */
       renderer.Clear();
 
+      /* ImGUI New Frame */
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+
+      glm::mat4 model = glm::translate( glm::mat4( 1.0f ), translation );
+      glm::mat4 mvp   = proj * view * model;
+
       shader.Bind();
       shader.SetUniform4f( "u_Color", r, 0.3f, 0.8f, 1.0f );
+      shader.SetUniformMat4f( "u_MVP", mvp );
 
       renderer.Draw( va, ib, shader );
 
@@ -115,6 +134,17 @@ int main( void )
 
       r += increment;
 
+      {
+        static float f = 0.0f;
+
+        ImGui::SliderFloat3( "Translation", &translation.x, 0.0f, 960.0f );
+        ImGui::Text( "Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate );
+      }
+
+      /* Render */
+      ImGui::Render();
+      ImGui_ImplOpenGL3_RenderDrawData( ImGui::GetDrawData() );
+
       /* Swap front and back buffers */
       glfwSwapBuffers( window );
 
@@ -123,6 +153,9 @@ int main( void )
     }
   }
 
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
   glfwTerminate();
   return 0;
 }
